95fe6c3402d561c74d23b4f581033417678b5ae1
raissanrbna <raissanirbana19@gmail.com>
2024-06-13 23:02:27 +0700

    update readme2

--- resources/views/table-point.blade.php
+++ resources/views/table-point.blade.php
@@ -4,7 +4,7 @@
     <div class="container mt-4">
         <div class="card shadow">
             <div class="card-header">
-                <h3>Data Point</h3>
+                <h3>Data Titik Wisata</h3>
             </div>
             <div class="card-body">
                 <table class="table table-boardered table-striped" id="example">
